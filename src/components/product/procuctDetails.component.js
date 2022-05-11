70bd4067d2994d4899a9f21c7ffd9bbf890388df
ashawin <ashwanimishra@Ashwanis-MacBook-Air.local>
2022-05-12 02:14:08 +0530

    product details

--- src/components/product/procuctDetails.component.js
+++ src/components/product/procuctDetails.component.js
@@ -1,11 +1,7 @@
 
 import React, { Component } from "react";
-import { useParams } from 'react-router-dom';
-
-const params = useParams();
 function Product() {
-    const {id} = useParams();
-    console.log('id',id)
+  
     return (
       <div className="App">
             
